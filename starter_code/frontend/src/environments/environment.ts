636796f6aacd0ac3970880df66a29054e4e04f0d
Miyachi, Christine <Christine.Miyachi@xerox.com>
2020-04-22 17:54:40 -0400

    fix after feedback

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'tomariken.auth0.com', // the auth0 domain prefix
+    url: 'tomariken', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'ypVeYkA1TRzchPNpE6j2Lvh76COwOHIT', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
